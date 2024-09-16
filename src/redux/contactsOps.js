33fec104cdac886c553b27701bc22edd29d9709d
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-09-16 10:42:09 +0300

    upd

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,110 @@
+// для зберігання асинхронних генераторів Екшенів (по прикладу з 5 ї ДЗ коли робили запити з бібліотеки на галерею)
+
+import axios from "axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { userAPI } from './userAPI'
+
+// First, create the thunk
+// const fetchUserById = createAsyncThunk(
+//     'users/fetchByIdStatus',
+//     async (userId: number, thunkAPI) => {
+//         const response = await userAPI.fetchById(userId)
+//         return response.data
+//     },
+// )
+
+// interface UsersState {
+//     entities: User[]
+//     loading: 'idle' | 'pending' | 'succeeded' | 'failed'
+// }
+
+// Оголоси наступні операції:
+
+// fetchContacts - одержання масиву контактів(метод GET) запитом.Базовий тип екшену це рядок "contacts/fetchAll".
+//     addContact - додавання нового контакту(метод POST).Базовий тип екшену це рядок "contacts/addContact".
+//         deleteContact - видалення контакту по ID(метод DELETE).Базовий тип екшену це рядок "contacts/deleteContact".
+// "tasks/fetchAll/pending" - початок запиту
+// "tasks/fetchAll/fulfilled" - успішне завершення запиту
+// "tasks/fetchAll/rejected" - завершення запиту з помилкою
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTA1YjQwYTViZDU5OTA1NDIyMGY0NDk3YmEzYjJmNSIsIm5iZiI6MTcyNTU5NDQyMy4yODc1ODgsInN1YiI6IjY2ZDllMWM1ZDhlODg1YWI4NDZkMDkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TvbdXQqddSub5Mw59Uixse-OSNQE4Xunk36NlBhj_AM' }
+});
+// початковий варіант запиту без відпрацювання станів з помилками:
+// export const fetchTasks = createAsyncThunk("contacts/fetchAll", async () => {
+//     const response = await axios.get("/contacts");
+//     return response.data;
+// });
+
+export const fetchTasks = createAsyncThunk(
+    "contacts/fetchAll",
+    // Використовуємо символ підкреслення як ім'я першого параметра, тому що в цій операції він нам не потрібен
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("/contacts");
+            // При успішному запиті повертаємо проміс із даними з бекенду
+            return response.data;
+        } catch (e) {
+            // При помилці запиту повертаємо проміс, який буде відхилений з текстом помилки
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+);
+
+//getProductMovies робить запит на УРЛ до SearchForm
+async function getProductsSerch(query, page = 1) {
+    const response = await instance.get(`/search/movie`,
+        {
+            params: {
+                page,
+                query,
+                include_adult: false,
+                language: "en-US",
+                region: "string",
+                year: "string"
+            },
+        },
+    )
+    // console.log(response.data.results)
+    return response.data.results;
+};
+export { getProductsSerch };
+
+//getProductMovies робить запит на УРЛ до HomePage
+export const getProductMovies = async () => {
+    const response = await instance.get('/trending/movie/day')
+    return response.data.results;
+};
+
+//getProductDetails робить запит на УРЛ з деталізацією про товар (додаю в окремий стан при монтуванні в компонент MovieDetailsPage)
+export const getProductDetails = async (movie_id) => {
+    const response = await instance.get(`/movie/${movie_id}`)
+    // console.log(response.data)
+    return {
+        title: response.data.title, poster_path: response.data.poster_path, release_date: response.data.release_date,
+        vote_average: response.data.vote_average, overview: response.data.overview, status: response.data.status,
+        // genres: { id: response.data.id, name: response.data.name },
+    }
+};
+
+export const getProductCast = async (movie_id) => {
+    const response = await instance.get(`/movie/${movie_id}/credits`, {
+        params: {
+            movie_id,
+        },
+    });
+    return response.data.cast;
+};
+
+export const getProductReviews = async (movie_id) => {
+    const response = await instance.get(`/movie/${movie_id}/reviews`, {
+        params: {
+            movie_id,
+        },
+    });
+
+    return response.data.results;
+};
+
+
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,19 +2,55 @@
 import { nanoid } from 'nanoid';
 import { createSlice } from '@reduxjs/toolkit';
 // export const selectContact = (state) => state.locale.items; // повертає шматок стану зі слайсу
-
+// додаю тут в обєкт початкового стану:loading: false, error: null ддя управління станом Лоадінгу при загрузці та Ерор при помилці
 const slice = createSlice({
     name: "contacts",
-    initialState: { items: [] },
+    initialState: {
+        items: [],
+        loading: false,
+        error: null
+    },
 
     reducers: {
-        addContact: (state, action) => {
-            state.items.push({
-                id: nanoid(),
-                name: action.payload.name,
-                number: action.payload.number,
-            })
+        addContact: (state, action) =>
+        // старий варіант без запиту з бекенду
+        // {
+        //     state.items.push({
+        //         id: nanoid(),
+        //         name: action.payload.name,
+        //         number: action.payload.number,
+        //     })
+        // },
+        // Додаємо обробку зовнішніх екшенів
+        // reducers: builder => {
+        //     builder
+        //         .addCase(fetchTasks.pending, (state, action) => { })
+        //         .addCase(fetchTasks.fulfilled, (state, action) => { })
+        //         .addCase(fetchTasks.rejected, (state, action) => { });
+        // },
+        //});
+
+        {
+            builder
+                .addContact(fetchTasks.pending, (state, action) => {
+                    state.items.push({
+                        // id: nanoid(),
+                        name: action.payload.name,
+                        number: action.payload.number,
+                    })
+                    state.isLoading = true;
+                })
+                .addContact(fetchTasks.fulfilled, (state, action) => {
+                    state.isLoading = false;
+                    state.error = null;
+                    state.items = action.payload;
+                })
+                .addContact(fetchTasks.rejected, (state, action) => {
+                    state.isLoading = false;
+                    state.error = action.payload;
+                });
         },
+
         deleteContact: (state, action) => {
             return {
                 ...state,
